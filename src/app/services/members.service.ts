f25a8028b813fb801ba0273f755687aa8d0d46a8
sean <j.sean1316@gmail.com>
2021-08-29 20:26:46 +0900

    [#5] axios read

--- src/app/services/members.service.ts
+++ src/app/services/members.service.ts
@@ -1,7 +1,7 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Injectable } from '@angular/core';
 import axios from 'axios';
 import { CommonService } from './common.service';
-
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +14,10 @@ export class MembersService {
     name: '',
     age: ''
   }
-  CommonService: any
+  CommonService: any;
   
   membersCreate() {
-    axios.post('http://localhost:3100/api/v1/members', this.member).then((response) => {
+    axios.post('http://localhost:4200/api/v1/members', this.members).then((response) => {
       console.log('Done membersCreate', response);
       this.membersRead();
     }).catch((error) => {
@@ -32,14 +32,21 @@ export class MembersService {
   }
 
   membersRead() {
-    this.members = [{
-      name: '홍길동',
-      age: 20
-    }, {
-      name: '춘향이',
-      age: 16
-    }];
-    console.log('Done membersRead', this.members);
+    axios.get('http://localhost:4200/api/v1/members').then((response) => {
+      console.log('Done membersRead', response);
+      this.members = response.data.members;
+    }).catch((error) => {
+      this.CommonService.axiosError(error);
+    });
+
+    // this.members = [{
+    //   name: '홍길동',
+    //   age: 20
+    // }, {
+    //   name: '춘향이',
+    //   age: 16
+    // }];
+    // console.log('Done membersRead', this.members);
   }
   
   membersUpdate() {
